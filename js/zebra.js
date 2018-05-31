000f23e8fe065e1f9506511fac336ad7e844fbb9
Xdoudou <xuyalilucky@qq.com>
2018-05-31 11:32:36 +0800

    '完成部分'

---
+++ js/zebra.js
@@ -0,0 +1,14 @@
+var mySwiperZebra = new Swiper('.zebra-wrapper', {
+    direction: 'vertical',
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true,
+    }
+})
+var mySwiperAnswer = new Swiper('.answer-wrapper', {
+    direction: 'horizontal',
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true,
+    }
+})
