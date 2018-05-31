94ef929783a04731f2c05a4d9f1f977aabd8d719
Xdoudou <xuyalilucky@qq.com>
2018-06-01 01:38:22 +0800

    '动画'

--- js/zebra.js
+++ js/zebra.js
@@ -6,14 +6,6 @@ var mySwiperZebra = new Swiper('.zebra-wrapper', {
         crossFade: true,
     }
 })
-// 问答切换
-var mySwiperAnswer = new Swiper('.answer-wrapper', {
-    direction: 'horizontal',
-    effect: 'fade',
-    fadeEffect: {
-        crossFade: true,
-    }
-})
 
 // 结果切换
 var mySwiperAnswer = new Swiper('.result-wrapper', {
@@ -55,13 +47,108 @@ function Switch(index) {
 }
 
 //开启反套路
-$('.homePage-but').click(function(){
+$('.homePage-but').click(function () {
     Switch(2)
 })
 
 //选择男女进入选择题
+var sex = null;
 $('.gender-girl-but').click(switchChoice)
 $('.gender-boy-but').click(switchChoice)
-function switchChoice(){
+function switchChoice() {
+    var attr = $(this).attr("class")
+    if (attr == 'gender-boy-but') {
+        sex = 'boy'
+    } else {
+        sex = 'girl'
+    }
     Switch(3)
 }
+
+//选择题目
+var off = true;
+$('.click-boyanswer li').click(switchItem)
+function switchItem() {
+    if (!off) {
+        return false;
+    }
+    off = false;
+    var url = "url(./img/shape" + ($(this).index() + 1) + ".png)no-repeat left center",
+        befUrl = $(this).css('background')
+    index = mySwiperZebra.activeIndex - 2,
+        oThis = $(this);
+
+    //改变css样式
+    $(this).css({
+        "color": "#f54b24",
+        "background": url
+    })
+    //表情出现
+    var face = '.answer-page' + index + '-face';
+    $(face).addClass("answer-page" + index + "-face-active")
+    setTimeout(function () {
+        Switch(mySwiperZebra.activeIndex + 1)
+        setTimeout(function () {
+            $(face).removeClass("answer-page" + index + "-face-active")
+            oThis.css({
+                "color": "#373737",
+                "background": befUrl
+            });
+            off = true;
+        }, 3000)
+    }, 1000)
+
+}
+
+//最后一题，跳转页面
+$('.click-boyanswer5 li').click(function () {
+    if (!off) {
+        return false;
+    }
+    off = false;
+    var url = "url(./img/shape" + ($(this).index() + 1) + ".png)no-repeat left center",
+        index = mySwiperZebra.activeIndex - 2,
+        befUrl = $(this).css('background')
+    oIdex = $(this).index(),
+        oThis = $(this);
+
+    //改变css样式
+    $(this).css({
+        "color": "#f54b24",
+        "background": url
+    })
+    //表情出现
+    var face = '.answer-page' + index + '-face';
+    $(face).addClass("answer-page" + index + "-face-active")
+
+
+    setTimeout(function () { resultFn(oIdex) }, 1000)
+    function resultFn(oIdex) {
+        if (sex == 'boy' && oIdex == 0) {
+            var goIndex = mySwiperZebra.activeIndex + 1
+        } else if (sex == 'boy' && oIdex == 1) {
+            var goIndex = mySwiperZebra.activeIndex + 2
+        } else if (sex == 'boy' && oIdex == 2) {
+            var goIndex = mySwiperZebra.activeIndex + 3
+        } else if (sex == 'girl' && oIdex == 0) {
+            var goIndex = mySwiperZebra.activeIndex + 4
+        } else if (sex == 'girl' && oIdex == 1) {
+            var goIndex = mySwiperZebra.activeIndex + 5
+        } else if (sex == 'girl' && oIdex == 2) {
+            var goIndex = mySwiperZebra.activeIndex + 6
+        }
+
+        Switch(goIndex)
+
+        setTimeout(function () {
+            $(face).removeClass("answer-page" + index + "-face-active")
+            oThis.css({
+                "color": "#373737",
+                "background": befUrl
+            });
+            off = true;
+        }, 3000)
+    }
+
+
+})
